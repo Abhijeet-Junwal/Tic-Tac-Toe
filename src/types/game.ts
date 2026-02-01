6c59014428f9fc14af5247aac92f55429901c779
Abhijeet-Junwal <abhijeetjunwal@gmail.com>
2026-02-01 23:28:43 +0530

    setup game state structure

---
+++ src/types/game.ts
@@ -0,0 +1,20 @@
+export type Symbol = "X" | "O";
+export type Players = "single" | "double";
+export type Mode = "normal" | "giiker";
+
+export type Move = {
+    index : number;
+};
+
+export type PlayerState = {
+    symbol: Symbol;
+    moves: Move[];
+};
+
+export type GameState = {
+    board: (String | null)[];
+    currentTurn: Symbol;
+    players: Record<Symbol, PlayerState>;
+    mode: Mode;
+    winner: Symbol | null;
+};
